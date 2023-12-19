af390d3fd86447cba855819bcf6a1546d5ba406e
Nikita Reznikov <reznikov.na@phystech.edu>
2023-12-19 17:49:46 +0300

    Update views

--- service/models.py
+++ service/models.py
@@ -2,6 +2,7 @@ import json
 import os
 import pickle
 import typing as tp
+import numpy as np
 
 from pydantic import BaseModel
 
@@ -45,6 +46,54 @@ class LightFM:
                 self.recommendations = json.load(f)["item_id"]
         else:
             self.recommendations = {"0": [1, 2, 3]}  # Заглушка для теста
+        self.popular = TopPopular().recommends
+
+    def recommend(self, user_id: int) -> tp.List[int]:
+        if str(user_id) in self.recommendations:
+            return self.recommendations[str(user_id)]
+        return self.popular
+
+
+class DSSM:
+    def __init__(self) -> None:
+        file_path = os.path.join(os.getcwd(), "service/saved_models/dssm_recommendations.json")
+        if os.path.exists(file_path):
+            with open(file_path, "r", encoding="utf-8") as f:
+                self.recommendations = json.load(f)["item_id"]
+        else:
+            self.recommendations = {}
+        self.popular = TopPopular().recommends
+
+    def recommend(self, user_id: int) -> tp.List[int]:
+        if str(user_id) in self.recommendations:
+            return self.recommendations[str(user_id)]
+        return self.popular
+
+
+class AutoEncoder:
+    def __init__(self) -> None:
+        file_path = os.path.join(os.getcwd(), "service/saved_models/ae_recommendations.json")
+        if os.path.exists(file_path):
+            with open(file_path, "r", encoding="utf-8") as f:
+                self.recommendations = json.load(f)["item_id"]
+        else:
+            self.recommendations = {}
+        self.popular = TopPopular().recommends
+
+    def recommend(self, user_id: int) -> tp.List[int]:
+        if str(user_id) in self.recommendations:
+            return self.recommendations[str(user_id)]
+        return self.popular
+
+
+class MultiVae:
+    def __init__(self) -> None:
+        file_path = os.path.join(os.getcwd(), "service/saved_models/multivae_recommendations.json")
+        if os.path.exists(file_path):
+            with open(file_path, "r", encoding="utf-8") as f:
+                self.recommendations = json.load(f)["item_id"]
+        else:
+            self.recommendations = {}
         self.popular = TopPopular().recommends
 
     def recommend(self, user_id: int) -> tp.List[int]:
